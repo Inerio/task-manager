b47cbf38d550ba5a80a8f670ff199640650b25b7
Inerio <julien.dearaujo.pro@gmail.com>
2025-08-18 14:29:35 +0200

    chore: Front config prod

---
+++ task-manager-front/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: "/api/v1",
+  clientIdHeader: "X-Client-Id",
+  uploadAcceptTypes: "image/*,application/pdf",
+  uploadMaxBytes: 5 * 1024 * 1024, // 5 MB
+};
